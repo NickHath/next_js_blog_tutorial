d17ba9f4d46d167573c5ee90520aa85d2632cd90
NickHath <Nicholashathaway@gmail.com>
2019-06-30 06:21:28 -0600

    add contentful helper and boilerplate for homepage

---
+++ helpers/getContent.js
@@ -0,0 +1,37 @@
+import fs from 'fs';
+import path from 'path';
+import { createClient } from 'contentful';
+
+const SPACE = process.env.CONTENTFUL_SPACE
+const TOKEN = process.env.CONTENTFUL_TOKEN
+
+const client = createClient({
+  space: SPACE,
+  accessToken: TOKEN
+});
+
+const types = [
+  'pageHome'
+];
+
+export const getContent = async () => {
+  types.forEach(() => {
+    const entries = await client.getEntries({
+      content_type: type,
+      include: 3
+    });
+
+    if (entries.total) {
+      const { fields } = entries.items[0];
+      fs.writeFileSync(
+        path.join(__dirname, '..', 'data', `${type.json}`),
+        JSON.stringify(fields)
+      );
+    }
+  });
+  return true;
+}
+
+if (process.argv[2] === 'install') {
+  getContent();
+}
